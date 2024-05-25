00648f06a7884a2e35ae074771951bc0c867f37a
GabrielR711 <gabrielrobles2011@hotmail.com.ar>
2024-05-25 12:17:06 -0300

    Home

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { HomeBall } from "./pages/home/HomeBall";
@@ -16,7 +8,6 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
->>>>>>> Stashed changes
   <React.StrictMode>
     <HomeBall />
   </React.StrictMode>
